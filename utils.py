c2e67c5ba86fa748c4a1cfe138ab087f56a5f3c5
ananyasharma20 <112490009+ananyasharma20@users.noreply.github.com>
2023-12-12 19:20:50 -0500

    Update utils.py

--- utils.py
+++ utils.py
@@ -10,6 +10,10 @@ from torchvision import ops
 import torch.nn.functional as F
 import torch.optim as optim
 
+# We referenced the following code while writing this file: 
+# https://towardsdatascience.com/understanding-and-implementing-faster-r-cnn-a-step-by-step-guide-11acfff216b0
+# https://github.com/wingedrasengan927/pytorch-tutorials/tree/master/Object%20Detection
+
 # -------------- Data Untils -------------------
 
 def parse_annotation(annotation_path, image_dir, img_size):
